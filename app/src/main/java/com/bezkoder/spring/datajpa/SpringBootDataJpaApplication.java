cf918267e6091bcefcaf4ab9b313f10c7ce98c65
Dmitry Chistyakov <dchistyakov@reksoft.ru>
2023-05-28 22:34:31 +0300

    first

--- app/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ app/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -1,13 +1,26 @@
 package com.bezkoder.spring.datajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import com.bezkoder.spring.datajpa.storage.StorageProperties;
+import com.bezkoder.spring.datajpa.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
--- app/src/main/java/com/bezkoder/spring/datajpa/database/UseForUser.java
+++
@@ -1,6 +0,0 @@
-package com.bezkoder.spring.datajpa.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UseForUser extends CrudRepository<User, Long> {
-}
